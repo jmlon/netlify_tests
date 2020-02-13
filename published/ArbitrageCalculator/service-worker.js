b01af25f1c6f55acaabf85691693248294ba2d71
Jorge Londoño <jorge.londono@gmail.com>
2020-02-12 21:12:27 -0500

    Updates

--- published/ArbitrageCalculator/service-worker.js
+++ published/ArbitrageCalculator/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64b005b033e8f8eeb26752f2a5dc8e33.js"
+  "./precache-manifest.1edda6c44901ceccf4723d4d83558be7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
