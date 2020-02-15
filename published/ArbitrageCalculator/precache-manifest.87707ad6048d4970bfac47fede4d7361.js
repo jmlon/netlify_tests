f7a8f06b0fd9f57b8cb6604800ef2ea46cf4ad12
Jorge Londoño <jorge.londono@gmail.com>
2020-02-15 12:04:50 -0500

    UPdates

---
+++ published/ArbitrageCalculator/precache-manifest.87707ad6048d4970bfac47fede4d7361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d1e2895f1452a464c3dd96adc695d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "04fbb4dfd3bb31d04b9d",
+    "url": "./static/css/main.b2407d72.chunk.css"
+  },
+  {
+    "revision": "cf444e96fb5f735cf7b4",
+    "url": "./static/js/2.a9dc5f7e.chunk.js"
+  },
+  {
+    "revision": "89a26c2dffe35752081740f199ff2a9e",
+    "url": "./static/js/2.a9dc5f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fbb4dfd3bb31d04b9d",
+    "url": "./static/js/main.2778f3e5.chunk.js"
+  },
+  {
+    "revision": "3454ac137a76bea09360",
+    "url": "./static/js/runtime-main.37cf4588.js"
+  }
+]);
